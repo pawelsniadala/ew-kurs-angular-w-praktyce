e8bbc668fbb3efb2d8e51e26dde692397b38c30d
Paweł Śniadała <biner.home@gmail.com>
2023-04-26 01:25:34 +0200

    Testowanie formularzy

--- projekt/src/app/components/task-list-item/task-list-item.component.spec.ts
+++ projekt/src/app/components/task-list-item/task-list-item.component.spec.ts
@@ -47,4 +47,21 @@ describe('TaskListItemComponent', () => {
     expect(editSpy).toHaveBeenCalled();
     expect(component.editMode).toEqual(true);
   });
+
+  it('should show task name inside input', () => {
+    const input = fixture.debugElement.query(By.css('.task-name-input'));
+    expect((input.nativeElement as HTMLInputElement).value).toEqual(
+      component.message
+    );
+  });
+
+  it('should update task name when input value changes', () => {
+    const inputElem = fixture.debugElement.query(By.css('.task-name-input'));
+    const elem = inputElem.nativeElement as HTMLInputElement;
+    elem.value = 'Changed value!';
+    inputElem.triggerEventHandler('input', {
+      target: elem
+    });
+    expect(component.message).toEqual('Changed value!');
+  });
 });
