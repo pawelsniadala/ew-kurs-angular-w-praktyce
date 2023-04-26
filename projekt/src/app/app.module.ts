dc57981756d123642c54a94aefc6809e85f0ba95
Paweł Śniadała <biner.home@gmail.com>
2023-04-26 02:30:05 +0200

    Testowanie formularza z ngModel

--- projekt/src/app/app.module.ts
+++ projekt/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListItemComponent } from './components/task-list-item/task-list-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TaskListItemComponent } from './components/task-list-item/task-list-ite
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projekt/src/app/components/task-list-item/task-list-item.component.spec.ts
+++ projekt/src/app/components/task-list-item/task-list-item.component.spec.ts
@@ -5,6 +5,10 @@ import {
 } from '@angular/core/testing';
 import { TaskListItemComponent } from './task-list-item.component';
 import { By } from '@angular/platform-browser';
+import {
+  FormsModule,
+  NgModel
+} from '@angular/forms';
 
 describe('TaskListItemComponent', () => {
   let component: TaskListItemComponent;
@@ -13,7 +17,7 @@ describe('TaskListItemComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [TaskListItemComponent],
-      imports: [],
+      imports: [FormsModule],
       providers: []
     }).compileComponents();
   }));
@@ -49,19 +53,33 @@ describe('TaskListItemComponent', () => {
   });
 
   it('should show task name inside input', () => {
-    const input = fixture.debugElement.query(By.css('.task-name-input'));
-    expect((input.nativeElement as HTMLInputElement).value).toEqual(
-      component.message
-    );
+    // const input = fixture.debugElement.query(By.css('.task-name-input'));
+    const input = fixture.debugElement.query(By.directive(NgModel));
+    return fixture.whenStable().then(() => {
+      expect((input.nativeElement as HTMLInputElement).value).toEqual(
+        component.message
+      );
+    });
   });
 
   it('should update task name when input value changes', () => {
-    const inputElem = fixture.debugElement.query(By.css('.task-name-input'));
+    // const inputElem = fixture.debugElement.query(By.css('.task-name-input'));
+    const inputElem = fixture.debugElement.query(By.directive(NgModel));
     const elem = inputElem.nativeElement as HTMLInputElement;
     elem.value = 'Changed value!';
     inputElem.triggerEventHandler('input', {
       target: elem
     });
     expect(component.message).toEqual('Changed value!');
+  });
+
+  it('should show error message when task name is empty', () => {
+    const inputElem = fixture.debugElement.query(By.directive(NgModel));
+    const model = inputElem.injector.get(NgModel);
+    // model.control.setValue('wartosc');
+    // fixture.detectChanges;
+    expect(model.valid).toBeFalsy();
+    const error = fixture.debugElement.query(By.css('.error'));
+    expect(error).not.toBeNull('Error message not found');
   });
 });
